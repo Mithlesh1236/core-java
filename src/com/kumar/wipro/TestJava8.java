0dcf3c448274826445e263e5e716be977f3bc9de
Mithlesh_1236 <kumar.mithlesh1236@gmail.com>
2023-09-21 23:02:10 -0500

    Adding some codes amex questions

--- src/com/kumar/wipro/TestJava8.java
+++ src/com/kumar/wipro/TestJava8.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
  * 1. Given a String, find the first non-repeated character in it using Java 8
  * 2. How to find duplicate elements in a given integers list in java 8
  * */
+
 public class TestJava8 {
 
     public static void main(String[] args) {
@@ -25,15 +26,9 @@ public class TestJava8 {
     //  Given a String, find the first non-repeated character in it using Java 8
     private static void firstNonRepeatedChar() {
         String input = "java";
-        Map<Character, Long> freqLinkedHashMap = input.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
+        Map<Character, Long> freqLinkedHashMap = input.chars().mapToObj(c -> (char) c).collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
 
-        Character result = freqLinkedHashMap
-                .entrySet().stream()
-                .filter(entry -> entry.getValue() == 1L)
-                .map(entry -> entry.getKey())
-                .findFirst().get();
+        Character result = freqLinkedHashMap.entrySet().stream().filter(entry -> entry.getValue() == 1L).map(entry -> entry.getKey()).findFirst().get();
 
         System.out.println("firstNonRepeatedChar = " + result);
     }
@@ -42,8 +37,7 @@ public class TestJava8 {
     private static void findDuplicates() {
         List<Integer> numbers = Arrays.asList(5, 3, 4, 1, 3, 7, 2, 9, 9, 4);
         Set<Integer> uniques = new HashSet<>();
-        Set<Integer> duplicates = numbers.stream()
-                .filter(n -> !uniques.add(n)) // Set.add() returns false if the element was already in the set.
+        Set<Integer> duplicates = numbers.stream().filter(n -> !uniques.add(n)) // Set.add() returns false if the element was already in the set.
                 .collect(Collectors.toSet());
 
         System.out.println("original list = " + numbers);
